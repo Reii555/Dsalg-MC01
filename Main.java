1333cbfd066b536cd999a1ecfb1b339377a2d994
pinky-ponky <panpansimbahan@gmail.com>
2026-01-25 02:22:03 +0800

    Called dibs on Selection Sort + set up Main.java for testing

--- Main.java
+++ Main.java
@@ -1,5 +1,99 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Use this method to run your experiments.
+        /* Available dataset files:
+            almostsorted.txt
+            random100.txt
+            random100000.txt
+            random25000.txt
+            random50000.txt
+            random75000.txt
+            totallyreversed.txt
+        */
+
+        // Initialize objects and variables used for measuring execution time
+        FileReader fileReader = new FileReader();
+        SortingAlgorithms sorter = new SortingAlgorithms();
+        long startTime, endTime, executionTimeMs;
+        double executionTimeSec;
+
+        // Change the filename here to test different datasets (Copy-paste from list above!)
+        String filePath = "data/";
+        String filename = "totallyreversed.txt";
+
+        Record[] records = fileReader.readFile(filePath + filename);
+
+        // Make copies so each algorithm gets the same unsorted input
+        Record[] arr1 = records.clone();
+        Record[] arr2 = records.clone();
+        Record[] arr3 = records.clone();
+        Record[] arr4 = records.clone();
+
+        System.out.println("\nSorting data file: " + filename + "\n");
+
+        // Just comment out other algorithms to test individually!
+        
+        /*** Insertion Sort ***/
+        System.out.println("============= Insertion Sort =============");
+        System.out.println("Is sorted? (Before): " + isSorted(arr1));
+        startTime = System.currentTimeMillis();
+        sorter.insertionSort(arr1, arr1.length);
+        endTime = System.currentTimeMillis();
+        System.out.println("Is sorted? (After): " + isSorted(arr1));
+        executionTimeMs = endTime - startTime; // Execution time in milliseconds
+        executionTimeSec = executionTimeMs / 1000.0; // Convert to seconds
+        System.out.println("Execution Time (in milliseconds): " + executionTimeMs + " ms");
+        System.out.println("Execution Time (in seconds): " + executionTimeSec + " s");
+        System.out.println("==========================================\n");
+
+        /*** Selection Sort ***/
+        System.out.println("============= Selection Sort =============");
+        System.out.println("Is sorted? (Before): " + isSorted(arr2));
+        startTime = System.currentTimeMillis();
+        sorter.selectionSort(arr2, arr2.length);
+        endTime = System.currentTimeMillis();
+        System.out.println("Is sorted? (After): " + isSorted(arr2));
+        executionTimeMs = endTime - startTime; // Execution time in milliseconds
+        executionTimeSec = executionTimeMs / 1000.0; // Convert to seconds
+        System.out.println("Execution Time (in milliseconds): " + executionTimeMs + " ms");
+        System.out.println("Execution Time (in seconds): " + executionTimeSec + " s");
+        System.out.println("==========================================\n");
+
+        /*** Merge Sort ***/
+        System.out.println("=============== Merge Sort ===============");
+        System.out.println("Is sorted? (Before): " + isSorted(arr3));
+        startTime = System.currentTimeMillis();
+        sorter.mergeSort(arr3, 0, arr3.length - 1);
+        endTime = System.currentTimeMillis();
+        System.out.println("Is sorted? (After): " + isSorted(arr3));
+        executionTimeMs = endTime - startTime; // Execution time in milliseconds
+        executionTimeSec = executionTimeMs / 1000.0; // Convert to seconds
+        System.out.println("Execution Time (in milliseconds): " + executionTimeMs + " ms");
+        System.out.println("Execution Time (in seconds): " + executionTimeSec + " s");
+        System.out.println("==========================================\n");
+
+        /*** Bubble Sort ***/
+        System.out.println("============== Bubble Sort ===============");
+        System.out.println("Is sorted? (Before): " + isSorted(arr4));
+        startTime = System.currentTimeMillis();
+        sorter.bubbleSort(arr4, arr4.length);
+        endTime = System.currentTimeMillis();
+        System.out.println("Is sorted? (After): " + isSorted(arr4));
+        executionTimeMs = endTime - startTime; // Execution time in milliseconds
+        executionTimeSec = executionTimeMs / 1000.0; // Convert to seconds
+        System.out.println("Execution Time (in milliseconds): " + executionTimeMs + " ms");
+        System.out.println("Execution Time (in seconds): " + executionTimeSec + " s");
+        System.out.println("==========================================\n");
+    }
+
+    /*
+     * Helper method to check if array is sorted
+     */
+    private static boolean isSorted(Record[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].getIdNumber() > arr[i + 1].getIdNumber()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -3,40 +3,45 @@
  */
 
 public class SortingAlgorithms {
-
     /*
      * You may define additional helper functions here if you need to.
      * Make sure the helper functions have the private access modifier, as
      * they will only be used in this class.
      */
 
+    // Assigned to:
     public void insertionSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
 
     }
 
-    // Rei
+    // Assigned to: Patricia Nicole S. Simbahan
     public void selectionSort(Record[] arr, int n) {
-        // TODO: Implement this sorting algorithm here.
-
+        for (int i = 0; i < n - 1; i++) { // Iterate through the whole array
+            int minIndex = i; // Assume the current index is the smallest
+            for (int j = i + 1; j < n; j++) { // Check the rest of the array
+                // Update minIndex if a smaller element is found
+                if (arr[j].getIdNumber() < arr[minIndex].getIdNumber()) {
+                    minIndex = j;
+                }
+            }
+            // Swap the smallest element found with the first element of the unsorted part
+            Record temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
     }
 
+    // Assigned to:
     public void mergeSort(Record[] arr, int p, int r) {
         // TODO: Implement this sorting algorithm here.
 
     }
 
-    /*
-    * Rei
-     * Define AT LEAST ONE more sorting algorithm here, apart from the
-     * ones given above. Make sure that the method accepts an array of
-     * records
-     */
-    public void bubbleSort(Record[] arr,) {
+    // Assigned to:
+    public void bubbleSort(Record[] arr, int n) {
         // TODO: Implement this sorting algorithm here.
 
     }
-
-
 }
 
