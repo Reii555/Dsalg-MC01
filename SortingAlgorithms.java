76fd38b3387b18d80a71c9eb394837ccea116069
Rei <SRLagoos@gmail.com>
2026-01-31 17:06:34 +0800

    Added bubble sort implementation

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -40,7 +40,26 @@ public class SortingAlgorithms {
 
     // Assigned to: Lagos
     public void bubbleSort(Record[] arr, int n) {
-        // TODO: Implement this sorting algorithm here.
+        boolean swapped;
+
+        //outer loop for each pass
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            //inner loop for comparing adjacent elements
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j].getIdNumber() > arr[j + 1].getIdNumber()) {
+                    //swaps arr[j] and arr[j+1]
+                    Record temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            //if no two elements were swapped in the inner loop, then break
+            if (!swapped) {
+                break;
+            }
+        }
 
     }
 }
